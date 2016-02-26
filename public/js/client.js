d5abde0b231698c0cb0999b3395a7f47e3905b2a
wolfen601 <kevin-ho@live.ca>
2016-02-26 11:40:24 -0500

    Basic functionality with database

--- public/js/client.js
+++ public/js/client.js
@@ -2,190 +2,158 @@
  * Client side operations in generating the visuals as well as functionality
 */
 document.addEventListener("DOMContentLoaded", function() {
-   var mouse = {
-      click: false,
-      move: false,
-      pos: {x:0, y:0},
-      pos_prev: false
-   };
-   // get canvas element and create context
-   var chatWndw  = document.getElementById('chat');
-   var canvas  = document.getElementById('drawing');
-   var context = canvas.getContext('2d');
-   var messageWindow = document.getElementById('messageWindow');
-   messageWindow.style.display = 'none';
-   var chatWidth   = 300;
-   var canvasWidth   = window.innerWidth - chatWidth - 20 || 720;
-   var canvasHeight  = 720;
+   var username = window.location.pathname.match(/\/user\/(.*)$/)[1];
    var socket  = io.connect();
-
-   //color picker
-   var colorPicker = document.getElementById('colorpicker');
-   var color = "#FF0000";
-
-   //eraser
-   var clear = document.getElementById('clear');
-   var cleared = false;
-   //save
-   var save = document.getElementById('save');
-   var saved = false;
-
-   //login
-   var login = document.getElementById('enter');
-   var loginSuccess = false;
-   var name = document.getElementById('name');
-   var username = "";
-
+   socket.emit('load', { username : username });
    //chat
    var send = document.getElementById('send');
    var sent = false;
+   var logout = document.getElementById('logout');
+   var newTask = document.getElementById('newTask');
 
-   //width/height
-   canvas.width = canvasWidth;
-   canvas.height = canvasHeight;
-   chatWndw.width = chatWidth;
+	send.onclick = function(){
+		sent = true;
+	};
 
-   //color
-   context.lineWidth = 2;
-   context.strokeStyle="#FF0000";
+  newTask.onclick = function(){
+    alert('hi');
+  };
 
-   // register mouse event handlers
-   canvas.onmousedown = function(e){ mouse.click = true; };
-   canvas.onmouseup = function(e){ mouse.click = false; };
+  logout.onclick = function(){
+    var myRows = [];
+    var headersText = [];
+    var $headers = $("th");
 
-   canvas.onmousemove = function(e) {
-      // normalize mouse position to range 0.0 - 1.0
-      mouse.pos.x = e.clientX / canvasWidth;
-      mouse.pos.y = e.clientY / canvasHeight;
-      mouse.move = true;
-   };
+    // Loop through grabbing everything
+    var $rows = $("tbody tr").each(function(index) {
+      $cells = $(this).find("td");
+      myRows[index] = {};
 
-   //ON CLICKS
-   clear.onclick = function(){ cleared = true; };
-
-   save.onclick = function() {
-     saved = true;
-     var canvas = document.getElementById("drawing");
-     var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-     window.location.href=image;
-   };
-
-   colorPicker.onclick = function(){
-     var canvas  = document.getElementById('drawing');
-     var context = canvas.getContext('2d');
-     var randomColor;
-     randomColor = Math.random() * 0x1000000; // 0 < randomColor < 0x1000000
-     randomColor = Math.floor(randomColor); // 0 < randomColor <= 0xFFFFFF
-     randomColor = randomColor.toString(16); // hex representation randomColor
-     randomColor = ("000000" + randomColor).slice(-6); // leading zeros added
-     randomColor = "#" + randomColor; // # added
-     context.strokeStyle=randomColor;
-     color = randomColor;
-   };
-
-   enter.onclick = function(){
-     username = name.value;
-     if(username == ""){
-        alert("Name invalid");
-     }else{
-       loginSuccess = true;
-     }
-   };
-
-   send.onclick = function(){ sent = true;};
-
-   //SOCKETS
-
-     // draw line received from server
-  	socket.on('drawLine', function (data) {
-        var line = data.line;
-        context.beginPath();
-        context.moveTo(line[0].x * canvasWidth, line[0].y * canvasHeight);
-        context.lineTo(line[1].x * canvasWidth, line[1].y * canvasHeight);
-        context.strokeStyle=line[2];
-        context.stroke();
-    });
-    //clear screen
-    socket.on('clear', function (data) {
-      var canvas  = document.getElementById('drawing');
-      var context = canvas.getContext('2d');
-      context.clearRect(0, 0, canvas.width, canvas.height);
-    });
-    //show message
-    socket.on('showMessage', function(data){
-      var id = data.message[0];
-      var user = data.message[1];
-      var msg = data.message[2];
-      //check if login or message
-      if(id == 0){
-        //login
-        var chat = document.createElement("li");
-        chat.className = 'system';
-        var newMsg = document.createElement("P");
-        var bold = document.createElement("b");
-        var text = document.createTextNode(user + " has logged in.");
-        bold.appendChild(text);
-        newMsg.appendChild(bold);
-        chat.appendChild(newMsg);
-        document.getElementById("chats").appendChild(chat);
-      }else if(id == 1){
-        //message
-        var chat = document.createElement("li");
-        chat.className = 'you';
-        if(user == username){
-          chat.className = 'me';
+      $cells.each(function(cellIndex) {
+        // Set the header text
+        if(headersText[cellIndex] === undefined) {
+          headersText[cellIndex] = $($headers[cellIndex]).text();
         }
-        var character = document.createElement("div");
-        var bold = document.createElement("b");
-        var chrName = document.createTextNode(user);
-        bold.appendChild(chrName);
-        character.appendChild(bold);
-
-        var newMsg = document.createElement("P");
-        var text = document.createTextNode(" " + msg);
-        newMsg.appendChild(text);
-
-        chat.appendChild(character);
-        chat.appendChild(newMsg);
-        document.getElementById("chats").appendChild(chat);
-      }
-      $('#chatWindow').stop().animate({
-        scrollTop: $("#chatWindow")[0].scrollHeight
-      }, 800);
+        // Update the row object with the header/cell combo
+        myRows[index][headersText[cellIndex]] = $(this).text();
+      });
     });
 
+    // Let's put this in the object like you want and convert to JSON (Note: jQuery will also do this for you on the Ajax request)
+    var myObj = {
+        "tasks": myRows
+    };
+    alert(JSON.stringify(myObj));
+  };
+   //SOCKETS
+   socket.on('tasks', function(data){
+     //alert('user: ' + data.username + '\n' + JSON.stringify(data.tasks) + '\n' + data.tasks.length );
+     if(data.username == username){
+       taskList = data.tasks;
+       for (var i = 0; i < taskList.length; i++) {
+          drawRow(taskList[i]);
+       }
+     }
+     loadScores(taskList);
+   });
+  //show message
+  socket.on('showMessage', function(data){
+    var noMessage = document.getElementById('nomessages');
+    noMessage.style.display = "none";
+    var chatWindow = document.getElementById('chatWindow');
+    chatWindow.style.display = "block";
+    var user = data.message[0];
+    var msg = data.message[1];
+    //message
+    var chat = document.createElement("li");
+    chat.className = 'you';
+    if(user == username){
+      chat.className = 'me';
+    }
+
+    var character = document.createElement("b");
+    var chrName = document.createTextNode(user);
+    character.appendChild(chrName);
+
+    var newMsg = document.createElement("P");
+    var text = document.createTextNode("" + msg);
+    newMsg.appendChild(text);
+
+    var image = document.createElement("div");
+    image.className = "image";
+
+    var img = document.createElement("img");
+    img.src = "../img/unnamed.jpg";
+
+    image.appendChild(img);
+    image.appendChild(character);
+
+    chat.appendChild(image);
+    chat.appendChild(newMsg);
+    document.getElementById("chats").appendChild(chat);
+
+    $('#chatWindow').stop().animate({
+      scrollTop: $("#chatWindow")[0].scrollHeight
+    }, 800);
+  });
+  //draw row
+  function drawRow(task){
+    var row = $("<tr />")
+    $("#tableBody").append(row);
+    row.append($("<td>" + task.TaskId + "</td>"));
+    row.append($("<td>" + task.TaskName + "</td>"));
+    row.append($("<td>" + task.Difficulty + "</td>"));
+    row.append($("<td>" + task.Points + "</td>"));
+    row.append($("<td>" + task.CreatedOn + "</td>"));
+    row.append($("<td>" + task.Status + "</td>"));
+  }
+  //load scores
+  function loadScores(taskList){
+    var easyCount = 0;
+    var mediumCount = 0;
+    var hardCount = 0;
+    var points = 0;
+    var completedCount = 0;
+    var activeCount = 0;
+    var failedCount = 0;
+    for (var i = 0; i < taskList.length; i++) {
+       if(taskList[i].Difficulty == "Easy"){
+         easyCount ++;
+       }
+       if(taskList[i].Difficulty == "Medium"){
+         mediumCount ++;
+       }
+       if(taskList[i].Difficulty == "Hard"){
+         hardCount ++;
+       }
+       if(taskList[i].Status == "Complete"){
+         completedCount ++;
+         points = points + taskList[i].Points;
+       }
+       if(taskList[i].Status == "Active"){
+         activeCount ++;
+       }
+       if(taskList[i].Status == "Failed"){
+         failedCount ++;
+       }
+    }
+    $("#easyTasks").text(easyCount);
+    $("#mediumTasks").text(mediumCount);
+    $("#hardTasks").text(hardCount);
+    $("#numTasks").text((easyCount + mediumCount + hardCount));
+    $("#totalPoints").text(points);
+    $("#numComplete").text(completedCount);
+    $("#numActive").text(activeCount);
+    $("#numFailed").text(failedCount);
+  }
    // main loop, running every 25ms
    function mainLoop() {
-
-     if(cleared == true){
-       cleared = false;
-       socket.emit('clear', {id: 'clear'});
-     }
-     if(saved == true){
-       saved = false;
-       socket.emit('save', {image: canvas.toDataURL("image/png")});
-     }
-     if(loginSuccess == true){
-       loginSuccess = false;
-       var messageWindow = document.getElementById('messageWindow');
-       messageWindow.style.display = 'block';
-       var loginScreen = document.getElementById('loginScreen');
-       loginScreen.style.display = 'none';
-       socket.emit('login', { message: [ 0, username, "" ] });
-     }
      if(sent == true){
        sent = false;
        var messageContent = document.getElementById('message');
        var msg = messageContent.value;
-       socket.emit('sendMessage', { message: [ 1, username, msg ] });
+       socket.emit('sendMessage', { message: [ username, msg ] });
      }
-      // check if the user is drawing
-      if (mouse.click && mouse.move && mouse.pos_prev) {
-         // send line to to the server
-         socket.emit('drawLine', { line: [ mouse.pos, mouse.pos_prev, color ] });
-         mouse.move = false;
-      }
-      mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
       setTimeout(mainLoop, 25);
    }
    mainLoop();
--- server.js
+++ server.js
@@ -9,128 +9,136 @@
 //initialize all the required libraries and connections
 var express = require('express'),
     app = express(),
+    bodyParser = require('body-parser'),
     http = require('http'),
     socketIo = require('socket.io'),
-    aws = require('aws-sdk');
+    aws = require('aws-sdk'),
+    path = require('path');
 var server = http.createServer(app);
 var io = socketIo.listen(server);
-var port = process.env.PORT || 80;
-
+var port = process.env.PORT || 8080;
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 /*******************
  * AWS Initialize  *
 *******************/
-var accessKeyId =  process.env.AWS_ACCESS_KEY || "xxxxxx";
-var secretAccessKey = process.env.AWS_SECRET_KEY || "xxxxxx";
-var s3bucket = process.env.S3_BUCKET || "xx-xx-xx-xx";
 //configure the aws account information to be used
-aws.config.update({
-    accessKeyId: accessKeyId,
-    secretAccessKey: secretAccessKey
-});
-//set the desired s3 bucket to use
-var s3 = new aws.S3({
-      params: {Bucket: s3bucket}
-});
+// aws.config.update({
+//   region: "us-west-2",
+//   endpoint: "http://localhost:8000"
+// });
+//
+// var docClient = new AWS.DynamoDB.DocumentClient();
+//
+// var table = "Users";
+// var username = "Kevichino";
+// var password = "password";
+// var taskListId = 100001;
+//
+// var params = {
+//   TableName:table,
+//   Item:{
+//     "username": username,
+//     "password": hash,
+//     "taskListId": taskListId,
+//   }
+// };
+//
+// console.log("Adding a new item...");
+// docClient.put(params, function(err, data) {
+//   if (err) {
+//     console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+//   } else {
+//     console.log("Added item:", JSON.stringify(data, null, 2));
+//   }
+// });
+// //set the desired s3 bucket to use
+// var s3 = new aws.S3({
+//       params: {Bucket: s3bucket}
+// });
 
 /***************
  * Routing     *
 ***************/
 //homepage
+
+app.post('/login', function(req, res){
+  console.log(req.body.user.name);
+  console.log(req.body.user.passwd);
+  //check database
+  if(req.body.user.name == "Kevichino" && req.body.user.passwd == "password"){
+    res.redirect('/user/' + req.body.user.name);
+  }
+});
+
+app.post('/signup', function(req, res){
+  console.log(req.body.user.name);
+  console.log(req.body.user.passwd);
+  if(req.body.user.name !== "" && req.body.user.passwd !== ""){
+    //check database
+    if(req.body.user.name == "Kevichino"){
+      console.log('User exists!');
+    }else{
+      res.redirect('/');
+    }
+  }else{
+    console.log('Nothing Entered!');
+  }
+});
+
 app.get('/', function(req, res){
-	res.render('home');
+	res.sendFile(path.join(__dirname, '/views', 'home.html'));
 });
-//drawing page
-app.get('/draw', function(req, res){
-	res.render('draw');
+
+app.get('/login', function(req, res){
+	res.sendFile(path.join(__dirname, '/views', 'login.html'));
 });
-//bookshelf
-app.get('/bookshelf', function(req, res){
-	res.render('bookshelf');
+
+app.get('/signup', function(req, res){
+	res.sendFile(path.join(__dirname, '/views', 'signup.html'));
 });
-//book
-app.get('/book/:id', function(req, res){
-	res.render('book');
+
+app.get('/user/:id', function(req, res){
+	res.sendFile(path.join(__dirname, '/views', 'user.html'));
 });
-//event planner
-app.get('/event', function(req, res){
-	res.render('event');
-});
+
 /**********************
  * Start Application  *
 ***********************/
 //start the application
 server.listen(port);
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
 console.log("Server running on 127.0.0.1:" + port);
-//historical data and counter for image names
-var lineHistory = [];
 var messageHistory = [];
-var counter = 0;
-/***************
- *   Sockets   *
-***************/
+var taskList = [{
+    "TaskId":"123456",
+    "TaskName":"Task1",
+    "Difficulty":"Easy",
+    "Points":"2",
+    "CreatedOn":"25/02/2016",
+    "Status":"Complete"
+  },
+  {
+      "TaskId":"654321",
+      "TaskName":"Task2",
+      "Difficulty":"Hard",
+      "Points":"10",
+      "CreatedOn":"25/02/2016",
+      "Status":"Active"
+    }];
 //on successful connection from client to server
 io.on('connection', function (socket) {
-  /***************
-   * history  *
-  ***************/
-  //on successful connection, redraw the canvase and resend the messages to the
-  //new client
-  for (var i in lineHistory) {
-     socket.emit('drawLine', { line: lineHistory[i] } );
-  }
   for (var i in messageHistory) {
      socket.emit('showMessage', { message: messageHistory[i] } );
   }
-
-  /***************
-   * Bookshelf  *
-  ***************/
-
-
-  /***************
-   * Drawing  *
-  ***************/
-  //when server receives a draw operation, store in history and tell other
-  //clients to draw
-  socket.on('drawLine', function (data) {
-     lineHistory.push(data.line);
-     io.emit('drawLine', { line: data.line });
-  });
-  //on clear operation received, clear draw and message history and tell all
-  //clients to erase canvas
-  socket.on('clear', function(data){
-    lineHistory = [];
-    messageHistory = [];
-    io.emit('clear', { id: data.id });
-  });
-  //on save operation, store the image buffer stream as an base64 encoded png
-  //image to s3 with public read only access
-  //file name : image[counter].png
-  socket.on('save', function(data){
-    var params = {
-      Key: "image" + counter + ".png",
-      Body: new Buffer(data.image.replace(/^data:image\/\w+;base64,/, ""),'base64'),
-      ContentEncoding: 'base64',
-      ContentType: 'image/png',
-      ACL: 'public-read'
-    };
-    s3.putObject(params, function(errBucket, dataBucket) {
-      if (errBucket) {
-        console.log("Error uploading data: ", errBucket);
-      } else {
-        console.log("Success uploading data: ", dataBucket);
-      }
-    });
-    counter = counter + 1;
-  });
-  /***************
-   * Chatting  *
-  ***************/
-  //on login detected, tell all clients that a new user has joined
-  socket.on('login', function(data){
-    io.emit('showMessage', { message: data.message } );
+  socket.on('load', function(data){
+    console.log('user: ' + data.username + '\n' + JSON.stringify(taskList));
+    io.emit('tasks', { username: data.username, tasks: taskList } );
   });
   //on message send detected, send message to all clients
   socket.on('sendMessage', function(data){
